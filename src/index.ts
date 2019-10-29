8f7a277833b69fdee2734b17e0b3a166de169182
Marat <marat@MBP-Marat-2.Dlink>
2019-10-29 23:13:56 +0300

    port change 5000

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, {Application, Request, Response, NextFunction} from 'express';
 
-const port = 80;
+const port = 5000;
 const __dirname = '.';
 import path from 'path';
 
@@ -11,8 +11,8 @@ const add = (a: number, b: number): number => a + b;
 app.get('/', (req: Request, res: Response, nexp: NextFunction) => {
     console.log(add(5,5));
     // res.sendFile('../index.html');
-    res.sendFile('index.html', {root: __dirname});
-    // res.send('HELLO');
+    // res.sendFile('index.html', {root: __dirname});
+    res.send('HELLO');
 });
 
 app.listen(port, ()=>{
